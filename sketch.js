3cc7596cfa1e18bafad913de231835270b62d7b1
Crazyrabbitz007 <61986489+Crazyrabbitz007@users.noreply.github.com>
2020-09-29 14:58:05 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,41 @@
+// my pets
+let pets = [{
+  name: "Scruffy",
+  color: "Brown"
+}, {
+  name: "Boxy",
+  color: "Light Brown"
+}, {
+  name: "Mandy",
+  color: "Black and White"
+}, {
+  name: "Blue",
+  color: "Deep Blue"
+}];
+
+let randomIndex;
+
 function setup() {
   createCanvas(400, 400);
+  background(70);
+
+  randomIndex = int(random(pets.length));
+
+console.log(pets[randomIndex].name);
+text(pets[randomIndex].name,50,50);
+
+pets.splice(randomIndex,1);
+console.log(pets);
+
+  // console.log("initial array is ");
+  // console.log(pets);
+  //
+  // pets.splice(4,1);
+  // console.log("array after splice")
+  // console.log(pets);
+
 }
 
 function draw() {
-  background(70);
+
 }
